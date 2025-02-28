4e4a20af92a453b4a3c543ad773b8f0665c067aa
gaurav1Nn <gauravnilawar26@gmail.com>
2025-02-28 22:51:40 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
 import express from "express";
+import morgan from "morgan";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ;
+const PORT = process.env.PORT ;
+// logging middleware
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Body parser Middleware
+app.use(express.json({ limit: "10kb"}));
+app.use(express.urlencoded({ extended: true , limit : "10kb"}));
+ // GLOBAL error handler
+ app.use((err,req,res,next) => {
+          console.error(err.stack);
+          res.status(err.status || 500 ).json({
+              status: "error",
+              message: err.message || "Internal server error",
+              ...PORT(process.env,NODE_ENV) === "development" && {stack: err.stack},
+          });
+ });
+
+
 // it should be always at bottom 
 // 404 handler
 app.use((req,res)=> {
