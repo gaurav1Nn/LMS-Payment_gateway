9e2242c3b95cce9050bf2cb6f85ed1ca513a22d1
gaurav1Nn <gauravnilawar26@gmail.com>
2025-02-28 08:51:24 +0530

    updated Index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.PORT);
+
+const app = express();
+const port = process.env.PORT ;
+// it should be always at bottom 
+// 404 handler
+app.use((req,res)=> {
+    res.status(404).json({
+        status:"error",
+        message: "Route not found",
+    });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} , in ${process.env.NODE_ENV} mode`);
+});
+
